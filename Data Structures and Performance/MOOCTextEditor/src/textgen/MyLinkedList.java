323c8858ca5a78cd16f9c897ad9ecda1372b9386
Dominik Sudwischer <dominik.sudwischer@gmail.com>
2018-01-26 21:50:22 +0100

    First lines of the Linked List implementation

--- Data Structures and Performance/MOOCTextEditor/src/textgen/MyLinkedList.java
+++ Data Structures and Performance/MOOCTextEditor/src/textgen/MyLinkedList.java
@@ -2,6 +2,8 @@ package textgen;
 
 import java.util.AbstractList;
 
+import javax.swing.text.AbstractDocument.LeafElement;
+
 
 /** A class that implements a doubly linked list
  * 
@@ -16,16 +18,29 @@ public class MyLinkedList<E> extends AbstractList<E> {
 
 	/** Create a new empty LinkedList */
 	public MyLinkedList() {
-		// TODO: Implement this method
+		head = new LLNode<E>(null);
+		tail = new LLNode<E>(null);
+		size = 0;
+		head.next = tail;
+		tail.prev = head;
 	}
 
 	/**
 	 * Appends an element to the end of the list
 	 * @param element The element to add
 	 */
-	public boolean add(E element ) 
+	public boolean add(E element) // Why is the return type boolean..?
 	{
-		// TODO: Implement this method
+		if(element == null)
+		{
+			throw new NullPointerException("A NULL element cannot be added to the list.");
+		}
+		LLNode<E> newNode = new LLNode<E>(element);
+		newNode.prev = this.tail.prev;
+		newNode.prev.next = newNode;
+		newNode.next = this.tail;
+		this.tail.prev = newNode;
+		this.size += 1;
 		return false;
 	}
 
@@ -33,8 +48,12 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	 * @throws IndexOutOfBoundsException if the index is out of bounds. */
 	public E get(int index) 
 	{
-		// TODO: Implement this method.
-		return null;
+		if(!isValidIndex(index))
+		{
+			throw new IndexOutOfBoundsException("The requested index is beyond the bounds.");
+		}
+		LLNode<E> curNode = getNode(index);
+		return curNode.data;
 	}
 
 	/**
@@ -42,17 +61,27 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	 * @param The index where the element should be added
 	 * @param element The element to add
 	 */
-	public void add(int index, E element ) 
+	public void add(int index, E element) 
 	{
-		// TODO: Implement this method
+		if(isNull(element))
+		{
+			throw new NullPointerException("NULL elements cannot be inserted.");
+		}
+		LLNode<E> curNode = getNode(index);
+		// We insert the new Node straight before curNode
+		LLNode<E> newNode = new LLNode<E>(element);
+		newNode.prev = curNode.prev;
+		newNode.next = curNode;
+		curNode.prev.next = newNode;
+		curNode.prev = newNode;
+		this.size += 1;
 	}
 
 
 	/** Return the size of the list */
 	public int size() 
 	{
-		// TODO: Implement this method
-		return -1;
+		return this.size;
 	}
 
 	/** Remove a node at the specified index and return its data element.
@@ -63,8 +92,15 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	 */
 	public E remove(int index) 
 	{
-		// TODO: Implement this method
-		return null;
+		if(!isValidIndex(index))
+		{
+			throw new IndexOutOfBoundsException();
+		}
+		LLNode<E> curNode = getNode(index);
+		curNode.prev.next = curNode.next;
+		curNode.next.prev = curNode.prev;
+		this.size -= 1;
+		return curNode.data;
 	}
 
 	/**
@@ -76,9 +112,45 @@ public class MyLinkedList<E> extends AbstractList<E> {
 	 */
 	public E set(int index, E element) 
 	{
-		// TODO: Implement this method
-		return null;
-	}   
+		if(!isValidIndex(index)) { throw new IndexOutOfBoundsException(); }
+		int curIndex = 0;
+		LLNode<E> curNode = this.head.next;
+		while(curIndex < index)
+		{
+			curNode = curNode.next;
+			curIndex++;
+		}
+		E oldData = curNode.data;
+		curNode.data = element;
+		return oldData;
+	} 
+	
+	/** Returns the node at the specified index */
+	private LLNode<E> getNode(int index)
+	{
+		if(!isValidIndex(index)) { throw new IndexOutOfBoundsException(); }
+		int curIndex = 0;
+		LLNode<E> curNode = this.head.next; // Since 0 <= index < this.size, we have at least
+		// one non-dummy node.
+		while(curIndex < index)
+		{
+			curNode = curNode.next;
+			curIndex++;
+		}
+		return curNode;
+	}
+	
+	/** Checks if an element is null */
+	private boolean isNull(E element)
+	{
+		return element == null;
+	}
+	
+	/** Checks if the specified index is within bounds */
+	private boolean isValidIndex(int index)
+	{
+		return index >= 0 && index < this.size();
+	}
 }
 
 class LLNode<E> 
@@ -87,14 +159,20 @@ class LLNode<E>
 	LLNode<E> next;
 	E data;
 
-	// TODO: Add any other methods you think are useful here
-	// E.g. you might want to add another constructor
-
 	public LLNode(E e) 
 	{
 		this.data = e;
 		this.prev = null;
 		this.next = null;
+	}
+	
+	public LLNode(E e, LLNode<E> prevNode, LLNode<E> nextNode)
+	{
+		this.data = e;
+		this.next = nextNode;
+		this.prev = prevNode;
+		prevNode.next = this;
+		nextNode.prev = this;
 	}
 
 }
